c69677a80ca69e35dcfcb8ddfcbef4bb2db92397
nick <nick_php@163.com>
2019-05-27 00:12:07 +0800

    :sparkles: endless graceful restart

--- main.go
+++ main.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
 	"gin-restful-api/pkg/setting"
 	"gin-restful-api/routers"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
